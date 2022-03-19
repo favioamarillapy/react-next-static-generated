16d4ea8aa9d70746384252465a9067e28f51b9eb
favioamarillapy <53306199+favioamarillapy@users.noreply.github.com>
2022-03-18 22:34:18 -0300

    Configurated dark theme

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
+import { darkTheme } from '../themes';
+
 
 import { NextUIProvider } from '@nextui-org/react';
 
 function MyApp({ Component, pageProps }) {
 
   return (
-    <NextUIProvider>
+    <NextUIProvider theme={darkTheme}>
       <Component {...pageProps} />
     </NextUIProvider>
   )
